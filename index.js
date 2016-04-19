bb4cbcce2e2ae529547bd31ad2854d0c43c52413
Roger <roger22002988@hotmail.com>
2016-04-19 22:23:03 +0800

    none

--- index.js
+++ index.js
@@ -8,28 +8,29 @@ app.set('port', (process.env.PORT || 5000));
 mongodb.MongoClient.connect(mongodbURL, function(err, db) {
 	if (err) {
 		console.log(err);
-		} 
+		}
 		else {
 			myDB = db;
 			console.log('connection success');
-			}
-			});
+		}
+	});
 app.get('/', function(request, response) {
 	response.status(200).send('<html><body><H1>Hello World</H1></body></html>');
 	response.end();
 	});
 app.get('/api/test', function(request, response) {
-var collection = myDB.collection('my_data');
-collection.find({}).toArray(function(err, docs) {
-if (err) {
-response.status(406).end();
-} else {
-response.type('application/json');
-response.status(200).send(docs);
-response.end();
-}
-});
-});
+	var collection = myDB.collection('my_data');
+	collection.find({}).toArray(function(err, docs) {
+		if (err) {
+			response.status(406).end();
+			} 
+		else {
+			response.type('application/json');
+			response.status(200).send(docs);
+			response.end();
+			}
+		});
+	});
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+	console.log('Node app is running on port', app.get('port'));
+	});
